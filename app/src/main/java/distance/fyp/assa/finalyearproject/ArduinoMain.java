8f0c7499634cd453540e59d50661d17b6a1bc632
Assa Singh <assachana@gmail.com>
2017-03-02 09:04:57 +0000

    bluetooth data recieved and parsed

--- app/src/main/java/distance/fyp/assa/finalyearproject/ArduinoMain.java
+++ app/src/main/java/distance/fyp/assa/finalyearproject/ArduinoMain.java
@@ -4,9 +4,19 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
+import android.graphics.SurfaceTexture;
+import android.graphics.SurfaceTexture.OnFrameAvailableListener;
+import android.hardware.Camera;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CaptureRequest;
+import android.opengl.GLES20;
+import android.opengl.Matrix;
+import android.os.Build;
 import android.os.Handler;
+import android.os.ParcelUuid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
@@ -14,16 +24,32 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.vrtoolkit.cardboard.CardboardActivity;
+import com.google.vrtoolkit.cardboard.CardboardView;
+import com.google.vrtoolkit.cardboard.EyeTransform;
+import com.google.vrtoolkit.cardboard.HeadTransform;
+import com.google.vrtoolkit.cardboard.Viewport;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
 import java.util.UUID;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
 
 public class ArduinoMain extends AppCompatActivity {
 
-    Button btnOn, btnOff;
-    TextView txtArduino, txtString, txtStringLength, sensorView0, sensorView1, sensorView2, sensorView3;
+    TextView sensorView0, sensorView1, sensorView2, sensorView3, sensorView4, sensorView5, sensorView6, sensorView7;
     Handler bluetoothIn;
+
+    private static String TAG = "ARDUINO_ACTIVITY";
 
     final int handlerState = 0;                        //used to identify handler message
     private BluetoothAdapter btAdapter = null;
@@ -33,7 +59,7 @@ public class ArduinoMain extends AppCompatActivity {
     private ConnectedThread mConnectedThread;
 
     // SPP UUID service - this should work for most devices
-    private static final UUID BTMODULEUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    private static final UUID BTMODULEUUID = UUID.fromString("0000111f-0000-1000-8000-00805f9b34fb");
 
     // String for MAC address
     private static String address;
@@ -44,40 +70,55 @@ public class ArduinoMain extends AppCompatActivity {
 
         setContentView(R.layout.activity_arduino_main);
 
-        //Link the buttons and textViews to respective views
-        btnOn = (Button) findViewById(R.id.buttonOn);
-        btnOff = (Button) findViewById(R.id.buttonOff);
-        txtString = (TextView) findViewById(R.id.txtString);
-        txtStringLength = (TextView) findViewById(R.id.testView1);
         sensorView0 = (TextView) findViewById(R.id.sensorView0);
         sensorView1 = (TextView) findViewById(R.id.sensorView1);
         sensorView2 = (TextView) findViewById(R.id.sensorView2);
         sensorView3 = (TextView) findViewById(R.id.sensorView3);
+        sensorView4 = (TextView) findViewById(R.id.sensorView4);
+        sensorView5 = (TextView) findViewById(R.id.sensorView5);
+        sensorView6 = (TextView) findViewById(R.id.sensorView6);
+        sensorView7 = (TextView) findViewById(R.id.sensorView7);
+
 
         bluetoothIn = new Handler() {
             public void handleMessage(android.os.Message msg) {
                 if (msg.what == handlerState) {                                     //if message is what we want
-                    String readMessage = (String) msg.obj;                                                                // msg.arg1 = bytes from connect thread
-                    recDataString.append(readMessage);                                      //keep appending to string until ~
+                    String readMessage = (String) msg.obj;
+
+                    recDataString.append(readMessage);
+
                     int endOfLineIndex = recDataString.indexOf("~");                    // determine the end-of-line
                     if (endOfLineIndex > 0) {                                           // make sure there data before ~
-                        String dataInPrint = recDataString.substring(0, endOfLineIndex);    // extract string
-                        txtString.setText("Data Received = " + dataInPrint);
+                        String dataInPrint = recDataString.substring(0, endOfLineIndex);    // ex
                         int dataLength = dataInPrint.length();                          //get length of data received
-                        txtStringLength.setText("String Length = " + String.valueOf(dataLength));
 
-                        if (recDataString.charAt(0) == '#')                             //if it starts with # we know it is what we are looking for
-                        {
-                            String sensor0 = recDataString.substring(1, 5);             //get sensor value from string between indices 1-5
-                            String sensor1 = recDataString.substring(6, 10);            //same again...
-                            String sensor2 = recDataString.substring(11, 15);
-                            String sensor3 = recDataString.substring(16, 20);
+                        Log.d("DATA", recDataString.toString());
 
-                            sensorView0.setText(" Sensor 0 Voltage = " + sensor0 + "V");    //update the textviews with sensor values
-                            sensorView1.setText(" Sensor 1 Voltage = " + sensor1 + "V");
-                            sensorView2.setText(" Sensor 2 Voltage = " + sensor2 + "V");
-                            sensorView3.setText(" Sensor 3 Voltage = " + sensor3 + "V");
-                        }
+                        String[] data = recDataString.toString().split("\\+");
+
+                        String sensor0 = data[0];
+                        String sensor1 = data[1];
+                        String sensor2 = data[2];
+                        String sensor3 = data[3];
+                        String sensor4 = data[4];
+                        String sensor5 = data[5];
+                        String sensor6 = data[6];
+                        String sensor7 = data[7];
+
+                        sensorView0.setText("front = " + sensor0);    //update the textviews with sensor values
+                        sensorView1.setText("back  = " + sensor1);
+
+                        sensorView3.setText("right = " + sensor3);
+                        sensorView2.setText("left = " + sensor2);
+
+
+                        sensorView7.setText("front right = " + sensor7);
+                        sensorView4.setText("front left = " + sensor4);
+
+                        sensorView6.setText("back right = " + sensor6);
+                        sensorView5.setText("back left = " + sensor5);
+
+
                         recDataString.delete(0, recDataString.length());                    //clear all string data
                         // strIncom =" ";
                         dataInPrint = " ";
@@ -89,25 +130,40 @@ public class ArduinoMain extends AppCompatActivity {
         btAdapter = BluetoothAdapter.getDefaultAdapter();       // get Bluetooth adapter
         checkBTState();
 
-        btnOff.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mConnectedThread.write("0");    // Send "0" via Bluetooth
-                Toast.makeText(getBaseContext(), "Turn off LED", Toast.LENGTH_SHORT).show();
-            }
-        });
+        //getDeviceBTUUID();
 
-        btnOn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                mConnectedThread.write("1");    // Send "1" via Bluetooth
-                Toast.makeText(getBaseContext(), "Turn on LED", Toast.LENGTH_SHORT).show();
-            }
-        });
+
+    }
+
+    private void getDeviceBTUUID() {
+        BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+
+        Method getUuidsMethod = null;
+        try {
+            getUuidsMethod = BluetoothAdapter.class.getDeclaredMethod("getUuids", null);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+
+        ParcelUuid[] uuids = new ParcelUuid[0];
+        try {
+            uuids = (ParcelUuid[]) getUuidsMethod.invoke(adapter, null);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+        Log.d("UUID", uuids[0].toString());
+
+        /*for (ParcelUuid uuid: uuids) {
+            Log.d("UUID","UUID: " + uuid.getUuid().toString());
+        }*/
     }
 
     private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
 
-        return  device.createRfcommSocketToServiceRecord(BTMODULEUUID);
+        return device.createRfcommSocketToServiceRecord(BTMODULEUUID);
         //creates secure outgoing connecetion with BT device using UUID
     }
 
@@ -120,53 +176,52 @@ public class ArduinoMain extends AppCompatActivity {
 
         //Get the MAC address from the DeviceListActivty via EXTRA
         address = intent.getStringExtra(MainActivity.EXTRA_DEVICE_ADDRESS);
+        Log.d("ADDRESS", address);
 
         //create device and set the MAC address
         BluetoothDevice device = btAdapter.getRemoteDevice(address);
 
+
         try {
             btSocket = createBluetoothSocket(device);
-        } catch (IOException e) {
-            Toast.makeText(getBaseContext(), "Socket creation failed", Toast.LENGTH_LONG).show();
-        }
-        // Establish the Bluetooth socket connection.
-        try
-        {
             btSocket.connect();
         } catch (IOException e) {
-            try
-            {
-                btSocket.close();
-            } catch (IOException e2)
-            {
-                //insert code to deal with this
+            try {
+                btSocket = (BluetoothSocket) device.getClass().getMethod("createRfcommSocket", new Class[]{int.class}).invoke(device, 1);
+            } catch (IllegalAccessException e1) {
+                e1.printStackTrace();
+            } catch (InvocationTargetException e1) {
+                e1.printStackTrace();
+            } catch (NoSuchMethodException e1) {
+                e1.printStackTrace();
             }
+        }
+        // Establish the Bluetooth socket connection.
+        try {
+            btSocket.connect();
+            Toast.makeText(this, "Connected to: " + btSocket.getRemoteDevice(), Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+
         }
         mConnectedThread = new ConnectedThread(btSocket);
         mConnectedThread.start();
 
         //I send a character when resuming.beginning transmission to check device is connected
         //If it is not an exception will be thrown in the write method and finish() will be called
+
         mConnectedThread.write("x");
     }
 
     @Override
-    public void onPause()
-    {
+    public void onPause() {
         super.onPause();
-        try
-        {
-            //Don't leave Bluetooth sockets open when leaving activity
-            btSocket.close();
-        } catch (IOException e2) {
-            //insert code to deal with this
-        }
     }
 
     //Checks that the Android device Bluetooth is available and prompts to be turned on if off
     private void checkBTState() {
 
-        if(btAdapter==null) {
+        if (btAdapter == null) {
             Toast.makeText(getBaseContext(), "Device does not support bluetooth", Toast.LENGTH_LONG).show();
         } else {
             if (btAdapter.isEnabled()) {
@@ -176,6 +231,7 @@ public class ArduinoMain extends AppCompatActivity {
             }
         }
     }
+
 
     //create new class for connect thread
     private class ConnectedThread extends Thread {
@@ -191,7 +247,8 @@ public class ArduinoMain extends AppCompatActivity {
                 //Create I/O streams for connection
                 tmpIn = socket.getInputStream();
                 tmpOut = socket.getOutputStream();
-            } catch (IOException e) { }
+            } catch (IOException e) {
+            }
 
             mmInStream = tmpIn;
             mmOutStream = tmpOut;
@@ -206,6 +263,7 @@ public class ArduinoMain extends AppCompatActivity {
                 try {
                     bytes = mmInStream.read(buffer);            //read bytes from input buffer
                     String readMessage = new String(buffer, 0, bytes);
+                    Log.d("ARDUINO_DATA", readMessage);
                     // Send the obtained bytes to the UI Activity via handler
                     bluetoothIn.obtainMessage(handlerState, bytes, -1, readMessage).sendToTarget();
                 } catch (IOException e) {
@@ -213,13 +271,17 @@ public class ArduinoMain extends AppCompatActivity {
                 }
             }
         }
+
         //write method
         public void write(String input) {
             byte[] msgBuffer = input.getBytes();           //converts entered String into bytes
             try {
-                mmOutStream.write(msgBuffer);                //write bytes over BT connection via outstream
+                mmOutStream.write(msgBuffer);
+                mmOutStream.flush(); //write bytes over BT connection via outstream
+
             } catch (IOException e) {
                 //if you cannot write, close the application
+                e.printStackTrace();
                 Toast.makeText(getBaseContext(), "Connection Failure", Toast.LENGTH_LONG).show();
                 finish();
 
